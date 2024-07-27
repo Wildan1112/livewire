0cb8afa14177ccf19c772ce3884b30aa89059306
Wildan Malul Azam <malula0101@gmail.com>
2024-07-27 19:48:29 +0700

    add validate PostForm

--- app/Livewire/Forms/PostForm.php
+++ app/Livewire/Forms/PostForm.php
@@ -11,17 +11,17 @@ class PostForm extends Form
     // #[Rule(['required', 'string', 'min:3'])]
     // public string $title = '';
 
-    #[Rule(['required'])]
+    #[Rule(['required', 'min:3'])]
     public string $body = '';
 
     public function store()
     {
 
-        $validated = $this->validate();
+        // $validated = $this->validate();
 
-        $post = Auth::user()->posts()->create($validated);
+        $post = Auth::user()->posts()->create($this->validate());
 
-        // session()->flash('message', 'Post created successfully!');
+        // session()->flash('message', 'Post creatvalidateded successfully!');
         flash('Post created successfully!', 'success');
 
         $this->reset();
